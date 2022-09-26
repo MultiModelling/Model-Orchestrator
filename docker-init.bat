8ad816b2fe96a616f9af965a797896def8517584
ruduan.plug@tno.nl <ruduan.plug@tno.nl>
2022-09-26 14:00:16 +0200

    Initial Commit

---
+++ docker-init.bat
@@ -0,0 +1,4 @@
+wsl mkdir -p dags logs data plugins common
+wsl sudo chown 50000:50000 dags logs data plugins common
+wsl sudo chmod -R 777 dags logs data plugins common
+wsl echo -e "AIRFLOW_UID=$(id -u)" > .env
